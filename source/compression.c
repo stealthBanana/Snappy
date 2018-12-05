8b84b525193add959d0af59315c7df6e04982085
stealthBanana <44704953+stealthBanana@users.noreply.github.com>
2018-12-05 15:53:10 +0100

    getSize():
    problema di stampa dei caratteri dell'array size

--- source/compression.c
+++ source/compression.c
@@ -3,42 +3,36 @@
 //
 #include "../header/compression.h"
 
-char *getSize(FILE *fin){
+unsigned char *getSize(FILE *fin){
     fseek(fin, 0, SEEK_END);
     //int intValueOfSize = ftell(fin);
-    int intValueOfSize = 638;
+    unsigned int intValueOfSize = 638;
     fclose(fin);
     //calcolo quanti bit servono per scrivere il numero della dimensione del file
     int numberOfBits = (int)log2(intValueOfSize)+1;
     //calcolo quanti byte servono per scrivere il numero della dimensione del file
     int numberOfBytes = ceil((double)numberOfBits/7);
     //istanzio un array di char lungo quanto i byte necessari per scrivere la  dimensione del file
-    char *size;
-    size = malloc(sizeof(char)*numberOfBytes);
-    //azzero i valori della stringa
-    size[0] = 0;
-    /*for(int i = 0; i < numberOfBytes; i++)
-        size[i] = 0;*/
-    for(int i = 0; i < (8*numberOfBytes); i++){
-        int mask = 1 << i;
-        int index = i/8;
-        //ogni 8 bit setto il flag del varint a 1
-        if(i % 8 == 7){
-            if(i != (8*numberOfBytes -1))
-                size[index] = atoi(&size[index]) | (mask >> (index*8));
-            //se si tratta dell'ultimo bit setto il flag a 0
-            else
-                size[index] = atoi(&size[index]) & 0x7f;
-        }else {
-            //aggiungo al char il bit alla i-esima posizione della dimensione
-            size[index] = atoi(&size[index]) | ((mask & intValueOfSize) >> (index*8));
+    unsigned char *size;
+    size = (char*)calloc(sizeof(char)*numberOfBytes, sizeof(char)*numberOfBytes);
+    for(int i = 0; i < numberOfBytes; i++){
+        for(int j = 0; j < 7; j++){
+            int mask = 1 << i*8+j-i;
+            size[i] = (unsigned char) ((size[i] - (char)(0)) | ((mask & intValueOfSize) >> (i*8-i)));
         }
+        size[i] = (unsigned char)((size[i] - (char) (0)) | 0x80);
     }
+    size[numberOfBytes-1] = (unsigned char)((size[numberOfBytes-1] - (char) (0)) & 0x7f);
     return size;
 }
 
 void compress(FILE *fin, FILE *fout)
 {
-    printf("size: %s", getSize(fin));
+    unsigned char *size = getSize(fin);
+    printf("size[0]: %d = ", (size[0]+0));
+    printf("%c\n", (unsigned char)(size[0]+0));
+    printf("size[1]: %d = ", (size[1]+0));
+    printf("%c\n", (unsigned char)(size[1]+0));
+    free(size);
 }
 
