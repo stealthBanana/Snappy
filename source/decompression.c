5eb48393d4146b2c64e7221ea034781309e63b00
Grgic <manel.gf.95@gmail.com>
2019-01-15 18:47:23 +0100

    Corretto degli errori rispetto alla prima consegna finita, però non è ancora veramente finita

--- source/decompression.c
+++ source/decompression.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 unsigned long dim(unsigned long d){
     unsigned long c = d;
@@ -11,12 +12,12 @@ unsigned long dim(unsigned long d){
     //conto il nummero di byte usati nella codifica della dimensione
     while(c){
         n++;
-        c = c >> 8;
+        c = c >> 7;
     }
     unsigned long p = 0;
     //algoritmo per la trasformazione dei byte nella lunghezza del file
     for(int i = 0; i < n; i++){
-        p = (p << 7) | ((d >> (i*8)) & 0x7F);
+        p = (p << 7) | ((d >> (i*7)) & 0x7F);
     }
     return p;
 }
@@ -24,14 +25,14 @@ unsigned long dim(unsigned long d){
 unsigned long inv(unsigned long b){
     unsigned long inv = 0;
     while(b){
-        inv = (inv << 1) | (b & 1);
-        b = b >> 1;
+        inv = (inv << 8) | (b & 0xFF);
+        b = b >> 8;
     }
     return inv;
 }
 
 void literal(unsigned long x, FILE *fout){
-    int c;
+    char c;
     unsigned long lit = inv(x);
     while(lit){
         c = lit & 0xFF;
@@ -43,7 +44,7 @@ void literal(unsigned long x, FILE *fout){
 void oneMatch(unsigned int len, unsigned int offset, FILE *fout){
     char c;
     for(int i = 0; i < len; i++){
-        fseek(fout, -offset+i, SEEK_END);
+        fseek(fout, -offset, SEEK_END);
         c = fgetc(fout);
         fseek(fout, 0, SEEK_END);
         fputc(c, fout);
@@ -53,7 +54,7 @@ void oneMatch(unsigned int len, unsigned int offset, FILE *fout){
 void twoMatch(unsigned int len, unsigned int offset, FILE *fout){
     char c;
     for(int i = 0; i < len; i++){
-        fseek(fout, -offset+i, SEEK_END);
+        fseek(fout, -offset, SEEK_END);
         c = fgetc(fout);
         fseek(fout, 0, SEEK_END);
         fputc(c, fout);
@@ -61,53 +62,62 @@ void twoMatch(unsigned int len, unsigned int offset, FILE *fout){
 }
 
 void decompress(FILE *fin, FILE *fout) {
-    unsigned char c;
+    unsigned char c = fgetc(fin);
     unsigned long dimbits = 0;
     unsigned long dimension = 0;
-    unsigned char tag = 0;
+    char tag = 0;
     unsigned long input = 0;
-    unsigned length = 0;
+    unsigned int length = 0;
     unsigned int offset = 0;
-    if((c = fgetc(fin)) & 0x80 == 0){
+    if(((c & 0x80) >> 7) == 0){
         dimbits = dimbits | (c & 0x7F);
     }
     else {
-        while ((c = fgetc(fin)) & 0x80 == 1) {
+        while (((c & 0x80) >> 7) == 1) {
             dimbits = dimbits | (c & 0x7F);
             dimbits = dimbits << 7;
+            c = fgetc(fin);
         }
         dimbits = dimbits | (c & 0x7F);
     }
     dimension = dim(dimbits);
-    while(dimension){
+    while(dimension > 0){
         c = fgetc(fin);
         tag = c & 0x3;
         switch(tag){
             case 0:
-                length = c & 0xFC;
-                int a = length;
-                while(a){
-                    input = input << 8;
-                    input = input | fgetc(fin);
-                    a--;
+                length = ((c & 0xFC) >> 2) + 1;
+                int cycle = ceil((length*1.0)/4);
+                for(int i = 0; i < cycle; i++){
+                    int a = 4;
+                    if((i == 1) && (length%4!=0)){
+                        a = length%4;
+                    }
+                    while(a){
+                        input = input << 8;
+                        input = input | fgetc(fin);
+                        a--;
+                    }
+                    literal(input, fout);
+                    input = 0;
                 }
-                literal(input, fout);
-                dimension = dimension - length - 1;
+                dimension = dimension - length;
                 break;
             case 1:
-                length = c & 0x1c;
+                length = (c & 0x1c) + 4;
                 offset = ((c & 0xE0) << 3) | fgetc(fin);
                 oneMatch(length, offset, fout);
-                dimension = dimension - 2;
+                dimension = dimension - length;
                 break;
             case 2:
                 length = c & 0xFC;
                 offset = (fgetc(fin) << 8) & fgetc(fin);
                 twoMatch(length, offset, fout);
-                dimension = dimension - 3;
+                dimension = dimension - length;
                 break;
             default:
-                printf("4 byte match not allowed!");
+                printf("tag sbagliato");
+                dimension = 0;
                 break;
         }
     }
--- source/main.c
+++ source/main.c
@@ -3,20 +3,20 @@
 #include "../header/compression.h"
 #include "../header/decompression.h"
 
-void main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
     //2 arguments
-    if(argc == 2){
+    if(argc == 4){
         FILE *fin, *fout;
         //check if file exists
-        if((fin = fopen(argv[1], "rb")) && (fout = fopen(argv[2], "wb+"))) {
+        if((fin = fopen(argv[2], "rb")) && (fout = fopen(argv[3], "wb+"))) {
 
             //COMPRESS
-            if (strcmp(argv[0], "-c") == 0) {
+            if (strcmp(argv[1], "-c") == 0) {
                 compress(fin, fout);
 
             //DECOMPRESS
-            } else if (strcmp(argv[0], "-d") == 0) {
+            } else if (strcmp(argv[1], "-d") == 0) {
                 decompress(fin, fout);
             } else {
                 printf("Comando non valevole");
@@ -24,9 +24,9 @@ void main(int argc, char *argv[]) {
         }
         
     //1 argument
-    }else if(argc == 1){
+    }else if(argc == 2){
         //HELP
-        if(strcmp(argv[0], "-h") == 0){
+        if(strcmp(argv[1], "-h") == 0){
             printf("Utilizzo: [opzione] [input file] [output file]\n\n"
                    "opzioni:\n"
                    "-c\tcomprime il file di input nel file di output\n"
@@ -39,4 +39,5 @@ void main(int argc, char *argv[]) {
     }else{
         printf("Argomenti non validi, per saperne di piu inserire l'opzione \"-h\"");
     }
+    return 0;
 }
